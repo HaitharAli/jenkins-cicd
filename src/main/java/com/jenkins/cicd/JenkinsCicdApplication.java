84c369c76b878641ce2185561890e64603d44fe3
HaitharAli <haitharali.mohammedali@ideas2it.com>
2022-10-07 12:53:27 +0530

    commiting commands

--- src/main/java/com/jenkins/cicd/JenkinsCicdApplication.java
+++ src/main/java/com/jenkins/cicd/JenkinsCicdApplication.java
@@ -1,5 +1,6 @@
 package com.jenkins.cicd;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@Slf4j
 public class JenkinsCicdApplication {
 
     @GetMapping("/jenkins")
     public String getMessage() {
+        log.info("Entering into Jenkins get method!!!!");
         return "Welcome to Jenkins CI/CD pipeline!!!";
     }
 
